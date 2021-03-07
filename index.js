214ae7221265920d6d5645a5a3068e5d33cccb93
Gogi <gogion@protonmail.com>
2021-03-07 18:54:48 +0200

    added const equals + operators event listener displays opeator on screen

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-const screen = document.getElementById('screen');
-const numberBtns = document.querySelectorAll('.number-btns');
-const del = document.getElementById('del');
-const ac = document.getElementById('ac');
-const operators = document.querySelectorAll('.operators');
-
-
+const btn = document.querySelectorAll(".btn");
+const screen = document.getElementById("screen");
+const numberBtns = document.querySelectorAll(".number-btns");
+const del = document.getElementById("del");
+const ac = document.getElementById("ac");
+const equals = document.getElementById("equals");
+const operators = document.querySelectorAll(".operators");
+let displayValue = 0;
 
 // Event Listeners
-numberBtns.forEach(button => button.addEventListener('click', () => {
-  screen.textContent += button.textContent;  
-}));
+numberBtns.forEach((button) =>
+  button.addEventListener("click", () => {
+    screen.textContent += button.textContent;
+  })
+);
 
-operators.forEach(button => button.addEventListener('click', () => {
-  console.log('OPERATOR cliced!');
-}));
+operators.forEach((button) =>
+  button.addEventListener("click", () => {
+    if (button.textContent !== "=") {
+      screen.textContent += button.textContent;
+    }
+  })
+);
 
-ac.addEventListener('click' , () => {
+ac.addEventListener("click", () => {
   screen.textContent = 0;
 });
 
-del.addEventListener('click', () => {
+del.addEventListener("click", () => {
   screen.textContent = screen.textContent.slice(0, -1);
-  console.log('DEL clicked!');
+  console.log("DEL clicked!");
 });
 
+equals.addEventListener("click", () => {
+  // screen.textContent = ;          // TODO add operation result
+});
+
+// Functions
 
 function emptyScreen() {
-  return screen.textContent = "";
+  return (screen.textContent = "");
 }
-emptyScreen()
-
-// function backSpace(){
-//   screen.value = screen.value.slice(0, - 1);
-// }
+emptyScreen();
